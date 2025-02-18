9395f0a9713a121bab619fa072a35e330f5b3e04
Mariovalcen <mariovalcen@gmail.com>
2025-02-18 01:42:28 +0100

    Update terminal prompt to bold and refine terminal color palette for better Kali Linux aesthetic

--- assets/js/terminal-animation.js
+++ assets/js/terminal-animation.js
@@ -6,15 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
   terminalOutput.innerHTML = '';
 
   const terminalLines = [
-    { type: 'prompt', text: '(kali@kali)-[~/]' },
+    { type: 'prompt-line', text: '(kali@kali)-[~/]' },
     { type: 'command', text: '$ whoami' },
     { type: 'output', text: 'mariovalcen' },
-    { type: 'prompt', text: '(kali@kali)-[~/]' },
+    { type: 'prompt-line', text: '(kali@kali)-[~/]' },
     { type: 'command', text: '$ cat about_me' },
     { type: 'output', text: `Hello! I’m Mario, a psychologist who changed from analyzing human behavior to trying to understand the mysteries of the kernel.<br>I started my way in psychology, but my curiosity for technology made me to train as a full-stack web developer...` },
-    { type: 'prompt', text: '(kali@kali)-[~/]' },
+    { type: 'prompt-line', text: '(kali@kali)-[~/]' },
     { type: 'command', text: '$ cd projects' },
-    { type: 'prompt', text: '(kali@kali)-[~/projects]' },
+    { type: 'prompt-line', text: '(kali@kali)-[~/projects]' },
     { type: 'command', text: '$ ls -la' },
     { type: 'output', text: `total 20<br>drwxr-xr-x  4 mario mario 4096 Feb 17 13:21 .<br>drwxr-xr-x  6 mario mario 4096 Feb 16 12:15 ..<br>-rw-r--r--  1 mario mario 4096 Feb 14 11:05 exploit.py<br>-rw-r--r--  1 mario mario 2048 Feb 14 11:06 nmap_scan.log<br><span style="color: var(--background-light);">-rwxr-xr-x  1 mario mario 4096 Feb 14 11:07 </span><a href="https://github.com/mariovalcen" style="color: var(--link-executable-color); font-weight: bold; text-decoration: none;">run_project.sh</a><br><span style="color: var(--background-light);">-rwxr-xr-x  1 mario mario 2048 Feb 15 10:30 </span><a href="https://github.com/mariovalcen" style="color: var(--link-executable-color); font-weight: bold; text-decoration: none;">reverse_shell.sh</a>` }
   ];
